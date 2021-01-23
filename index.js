b64f03da5fd689746abf0b1d998752c019633c42
adamperry-hub <ambiguous144@gmail.com>
2021-01-22 16:15:20 -0800

    little farther

--- index.js
+++ index.js
@@ -209,11 +209,11 @@ Practice accessing data above by console.log-ing following items:
 
 //(1) Name of the first artist (0th index) in the array
 
-  console.log(artists[artists.length -20]);
- 
+// console.log(artists[0]['name']) "Amedeo Modigliani"
+
 //(2) Bio of the third artist (2nd index) in the array 
 
-  console.log(artists[artists.length -18]);
+  // console.log(artists[18]['bio'])
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
